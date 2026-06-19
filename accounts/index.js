deb966d666b17c8b684e672df20c9d339cf89a14
LucasDuarteV <lucasdv2733@gmail.com>
2026-06-19 15:47:22 -0300

    Inicialização do sistema

--- accounts/index.js
+++ accounts/index.js
@@ -2,3 +2,25 @@ const chalk = require('chalk')
 const inquirer = require('inquirer')
 
 const fs = require('fs')
+const { error } = require('console')
+
+operation()
+
+function operation(){
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: [
+            'Criar Conta',
+            'Consultar Saldo',
+            'Depositar',
+            'Sacar',
+            'Sair',
+        ]
+    }]).then((answer =>{
+        console.log(answer)
+    })).catch(error => {
+        console.log(error)
+    })
+}
